080119cfd705ff120fd4a195da8db27a57d12f42
Ahmed <ahmedcodes48@gmail.com>
2025-03-05 00:15:31 +0500

    fourth file added

---
+++ 4- index.js
@@ -0,0 +1,14 @@
+// ABOUT PATH , read more in docs about path
+
+// const path = require("path");
+
+// const a = path.extname("/node/index.js");
+// const b = path.extname("/node/index.js");
+
+// console.log(a);
+
+const os = require("os");
+
+console.log(os.hostname())
+
+// these are till this file are build in module
