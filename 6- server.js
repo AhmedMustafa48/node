a0357fe7151d0597af4e11b7b919ea1138848a34
Ahmed <ahmedcodes48@gmail.com>
2025-03-06 23:51:19 +0500

    code added

--- 6- server.js
+++ 6- server.js
@@ -4,4 +4,10 @@ const PORT= 2000;
 const hostname = "localhost"
 
 
+const server = http.createServer((request, response, next) => {
+  
+});
 
+server.listen(PORT, hostname, () => {
+  console.log(`Server is working on http://${hostname}:${PORT}`);
+});
