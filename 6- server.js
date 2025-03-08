e2b6a9f77e455c4c1350a13dd16b4c98cbe8c930
Ahmed <ahmedcodes48@gmail.com>
2025-03-08 22:06:54 +0500

    changes

--- 6- server.js
+++ 6- server.js
@@ -13,4 +13,6 @@ const server = http.createServer((request, response, next) => {
   response.end("<h1>Hello World</h1>");
 });
 
-
+server.listen(PORT, hostname, () => {
+  console.log(`Server is working on http://${hostname}:${PORT}`);
+});
