d483761d11b5206653fc6b95386aff0f1fdf67f3
Ahmed <ahmedcodes48@gmail.com>
2025-03-13 18:50:07 +0500

    latest push

--- 6- server.js
+++ 6- server.js
@@ -3,3 +3,14 @@ const http = require("http");
 const PORT= 2000;
 const hostname = "localhost"
 
+const server = http.createServer((request, response, next) => {
+  if (request.url === "/about") {
+    response.end("<h1>About Page</h1>");
+  }
+  // console.log(request.url)
+  response.end("<h1>Hello World</h1>");
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server is working on http://${hostname}:${PORT}`);
+});
